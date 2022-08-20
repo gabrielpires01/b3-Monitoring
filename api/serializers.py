c283513f6a65be88f84ad347e27c46d097178b89
Gabriel Pires <gabrielpires@poli.ufrj.br>
2022-08-20 00:14:18 -0300

    Feat: Add Serializer for Pipeline

---
+++ api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Pipeline
+
+class PipelineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pipeline
+        fields = ("id", "top_value", "bottom_value", "current_value", "symbol", "created_at")
