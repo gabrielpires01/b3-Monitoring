4f7bd8f46a531ef198138dfabebac352f032c13a
Gabriel Pires <gabrielpires@poli.ufrj.br>
2022-08-20 00:05:22 -0300

    Fix: Table Typos

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Pipeline(models.Model):
-    top_value = models.DecimalField(..., decimal_places=4)
-    bottom_value = models.DecimalField(..., decimal_places=4)
-    symbol = models.CharField()
-    current_value = models.DecimalField(..., decimal_places=4)
+    top_value = models.DecimalField(max_digits=20, decimal_places=2)
+    bottom_value = models.DecimalField(max_digits=20, decimal_places=4)
+    symbol = models.CharField(max_length=10)
+    current_value = models.DecimalField(max_digits=20 , decimal_places=4)
     created_at = models.DateField(auto_now_add=True)
