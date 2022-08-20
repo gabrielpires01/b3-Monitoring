cce3a391767c38dbcc8a7752cb5adfc52548e9b8
Gabriel Pires <gabrielpires@poli.ufrj.br>
2022-08-20 00:02:22 -0300

    Feat: Table for Pipelines

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pipeline(models.Model):
+    top_value = models.DecimalField(..., decimal_places=4)
+    bottom_value = models.DecimalField(..., decimal_places=4)
+    symbol = models.CharField()
+    current_value = models.DecimalField(..., decimal_places=4)
+    created_at = models.DateField(auto_now_add=True)
